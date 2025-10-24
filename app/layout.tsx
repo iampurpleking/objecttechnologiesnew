297fee9c91601a91a6f7e515ab047e563ae09398
bryght001 <brightchukwunwike@gmail.com>
2025-10-24 07:19:03 +0100

    google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,10 +36,9 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         }} />
       </head>
       <body className="font-inter antialiased">
-        <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MBSW5999"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+        {/* Google Tag Manager (noscript) */}
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MBSW5999" height="0" width="0" style="display:none;visibility:hidden"></iframe>` }} />
+        {/* End Google Tag Manager (noscript) */}
         <AuthProvider>
           <Navbar />
           {children}
