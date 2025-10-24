0a4eb17983d09d53d8237ba2670bf3fe750b39df
bryght001 <brightchukwunwike@gmail.com>
2025-10-24 06:51:18 +0100

    google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,13 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" data-scroll-behavior="smooth">
       <head>
+        <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MBSW5999');</script>
+<!-- End Google Tag Manager -->
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link 
@@ -27,6 +34,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         />
       </head>
       <body className="font-inter antialiased">
+        <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MBSW5999"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
         <AuthProvider>
           <Navbar />
           {children}
